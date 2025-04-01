275ca3996ad6cc7a687e21342383fd1faa974455
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-04-01 21:27:43 +0100

    ammend service to hit repo

--- src/services/CustomerService.ts
+++ src/services/CustomerService.ts
@@ -1,27 +1,28 @@
 // src/services/CustomerService.ts
 import { EventBus } from "../core/EventBus";
 import { CustomerRepository } from "../repository/CustomerRepository";
-import { IOrderHistory } from "../types/";
+import { OrderRepository } from "../repository/OrderRepository";
+import { CustomerOrder } from "../types/";
 import { DomainError } from "../utils/Error";
 
 export class CustomerService {
   constructor(
     private customerRepository: CustomerRepository,
+    private orderRepository: OrderRepository,
     private eventBus: EventBus
   ) {}
 
-  placeOrder(customerId: string, order: IOrderHistory): void {
+  placeOrder(customerId: string, order: CustomerOrder): void {
     const customer = this.customerRepository.getById(customerId);
     if (!customer) throw new DomainError("Customer not found");
 
-    customer.placeOrder(order);
-    this.customerRepository.update(customer); // Persist the change
+    this.orderRepository.save(order);
 
     this.eventBus.publish({
       type: "CustomerOrderCreated",
       payload: {
-        customerId: customer.id,
-        orderId: order.orderId,
+        customerId: order.customerId,
+        orderId: order.id,
         products: order.products,
       },
     });
