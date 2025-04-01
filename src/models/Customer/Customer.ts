912b9626807403f57de9c7d5b17bc5f07bf921bf
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-04-01 21:26:36 +0100

    remove business logic from model

--- src/models/Customer/Customer.ts
+++ src/models/Customer/Customer.ts
@@ -1,11 +1,10 @@
 // src/models/Customer.ts
-import { IOrderHistory, IShippingAddress } from "../../types";
+import { IShippingAddress } from "../../types";
 import { DomainError } from "../../utils/Error";
 import { ValidationUtils } from "../../utils/ValidationUtils";
 
 export class Customer {
   private email!: string;
-  private orderHistory: IOrderHistory[] = [];
 
   constructor(
     public readonly id: string,
@@ -20,19 +19,11 @@ export class Customer {
     return this.shippingAddress;
   }
 
-  getOrderHistory(): IOrderHistory[] {
-    return this.orderHistory;
-  }
-
   setEmail(newEmail: string): void {
     if (!ValidationUtils.validateEmail(newEmail)) {
       throw new DomainError("Invalid email format.");
     }
     this.email = newEmail;
-  }
-
-  placeOrder(order: IOrderHistory): void {
-    this.orderHistory.push(order);
   }
 
   getEmail(): string {
