186fa3a5dde64d482a473bf872adc4d30eee130b
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-07-04 20:50:43 +0100

    allow the searching of customers

--- src/repository/CustomerRepository.ts
+++ src/repository/CustomerRepository.ts
@@ -1,40 +1,41 @@
-// src/repository/CustomerRepository.ts
 import { ICustomerDataSource } from "../types/datasource";
 import { Customer } from "../models/Customer/Customer";
 import { DomainError } from "../utils/Error";
 
 export class CustomerRepository {
   private dataSource: ICustomerDataSource;
+  private customers: Customer[];
+
   /**
    * Constructor using dependency injection pattern.
    */
   constructor(dataSource: ICustomerDataSource) {
     this.dataSource = dataSource;
-  }
-
-  private loadCustomers(): Customer[] {
-    return this.dataSource.loadCustomers();
+    this.customers = this.dataSource.loadCustomers();
   }
 
   getById(customerId: string): Customer | undefined {
-    const customers = this.loadCustomers();
-    return customers.find((customer) => customer.id === customerId);
+    return this.customers.find((customer) => customer.id === customerId);
   }
 
   save(customer: Customer): void {
-    const customers = this.loadCustomers();
-    if (customers.some((existing) => existing.id === customer.id)) {
+    if (this.customers.some((existing) => existing.id === customer.id)) {
       throw new DomainError("Customer already exists");
     }
+    this.customers.push(customer);
   }
 
   update(customer: Customer): void {
-    const customers = this.loadCustomers();
-    const existingCustomer = customers.find(
+    const index = this.customers.findIndex(
       (existing) => existing.id === customer.id
     );
-    if (!existingCustomer) {
+    if (index === -1) {
       throw new DomainError("Cannot update non-existent customer");
     }
+    this.customers[index] = customer;
+  }
+
+  getAll(): Customer[] {
+    return this.customers;
   }
 }
